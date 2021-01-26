0c87bc8c34983fdfae0eb8731a80c06438f00d79
DESKTOP-NK61CTR\sjdoh <sjdoh@explainalpha.com>
2021-01-26 18:35:36 +0900

    mongodb세팅

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+const config = require('./config/key')
 
-mongoose.connect('mongodb+srv://devdoe153:qwe123@cluster0.h7zum.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(()=> console.log('MongoDB Connected....'))
-.catch(err => console.log(err+ ' Error ...'));
+.catch(err => console.log(err , ' Error ...'));
 
 app.get('/', (req, res) => res.send('Hello world!'))
+
+app.post('/register', (req, res) => {
+console.log('/register')
+    const user = new User(req.body);
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success: false, err});
+        return res.status(200).json({
+            success: true
+        });
+
+
+    });
+});
 
 app.listen(port, ()=> console.log(`Example app listeing on port  ${port}`))
--- models/User.js
+++ models/User.js
@@ -33,4 +33,4 @@ const userSchema =  mongoose.Schema({
 
 const User = mongoose.model('User', userSchema)
 
-model.export = User;
+module.exports = {User};
